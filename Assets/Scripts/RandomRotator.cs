f9a8bc708ffefa8f7367bcaad753e1ff25eff962
Kim <kimberlyasharpe@gmail.com>
2016-09-18 20:03:22 -0400

    Can shoot + Asteroid
    Added shooting ability, destroy shot when out of bounds, and rotation
    to environmental asteroid object

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour 
+{
+	public float tumble;
+
+	void Start ()
+	{
+		Rigidbody asteroidBody = GetComponent<Rigidbody> ();
+		asteroidBody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
