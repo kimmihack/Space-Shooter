20ed59ff394625038a6199ebf4b6a1357270cf2e
Kim <kimberlyasharpe@gmail.com>
2016-09-22 19:42:56 -0400

    Update again
    Added movable hazards, added explosions! Boom!

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+
+	void Start ()
+	{
+		
+	}
+
+	void SpawnWaves () 
+	{
+		
+	}
+
+}
