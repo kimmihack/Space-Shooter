7bd6dc6d7c09a7f7ac39fc9371c3867af36f00b6
Kim <kimberlyasharpe@gmail.com>
2016-10-24 11:14:10 -0400

    Added scrolling background
    Finalized adding in the different asteroids and scrolling background
    has been added + star particle system

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class GameController : MonoBehaviour 
 {
-	public GameObject hazard;
+	public GameObject[] hazards;
 	public Vector3 spawnValue;
 	public int hazardCount;
 	public float spawnWait;
@@ -48,6 +48,7 @@ public class GameController : MonoBehaviour
 			
 			for (int i = 0; i < hazardCount; i++) 
 			{
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
 				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (hazard, spawnPosition, spawnRotation);
